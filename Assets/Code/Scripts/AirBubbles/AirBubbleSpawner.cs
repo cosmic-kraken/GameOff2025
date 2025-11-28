ef0960a737e0e3d7f0b3e18cdbc1486be0c68555
HadiDragneel <hadi.sadeghi.m23@gmail.com>
2025-11-28 14:30:40 +0100

    Feature/turtle movement (#6)
    * Audio Manager Prefab added
    
    * WIP Air Bubbles
    
    - Improved bubble spawners
    - Cleaned up files
    - Added color to Lenny
    
    ---------
    
    Co-authored-by: MJ <58364193+MJKoedam@users.noreply.github.com>

---
+++ Assets/Code/Scripts/AirBubbles/AirBubbleSpawner.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class AirBubbleSpawner : MonoBehaviour
+{
+    [Header("Performance Settings")]
+    [SerializeField] private bool onlySpawnWhenVisible = true;
+    
+    [Header("Spawn Settings")]
+    [SerializeField] private List<GameObject> bubblePrefabs;
+    [SerializeField] private float spawnInterval = 0.5f;
+    [SerializeField] private int poolSize = 10;
+    [SerializeField] private float xSpawnRange = 0.5f;
+
+    private Coroutine spawnCoroutine;
+    private WaitForSeconds spawnWait;
+    private ObjectPool<AirBubble> bubblePool;
+
+    private void Awake() {
+        bubblePool = new ObjectPool<AirBubble>(
+            CreateBubble,
+            OnGetBubble,
+            OnReleaseBubble,
+            OnDestroyBubble,
+            true,
+            poolSize,
+            poolSize * 2
+        );
+    }
+
+    private void OnValidate() {
+        spawnWait = new WaitForSeconds(spawnInterval);
+    }
+    
+    private void OnEnable() {
+        spawnWait = new WaitForSeconds(spawnInterval);
+        if (!onlySpawnWhenVisible) {
+            if (spawnCoroutine != null) {
+                StopCoroutine(spawnCoroutine);
+            }
+            spawnCoroutine = StartCoroutine(SpawnBubbles());
+        }
+    }
+
+    private void OnBecameVisible() {
+        if (onlySpawnWhenVisible && spawnCoroutine == null) {
+            spawnWait = new WaitForSeconds(spawnInterval);
+            spawnCoroutine = StartCoroutine(SpawnBubbles());
+        }
+    }
+
+    private void OnBecameInvisible() {
+        if (onlySpawnWhenVisible && spawnCoroutine != null) {
+            StopCoroutine(spawnCoroutine);
+            spawnCoroutine = null;
+        }
+    }
+    
+    private IEnumerator SpawnBubbles() {
+        while (true) {
+            yield return spawnWait;
+
+            var spawnPosition = transform.position;
+            spawnPosition.x += Random.Range(-xSpawnRange, xSpawnRange);
+            var bubble = bubblePool.Get();
+            bubble.Init(bubblePool, spawnPosition);
+            bubble.gameObject.SetActive(true);
+        }
+    }
+    
+    private void OnDisable() {
+        if (spawnCoroutine != null) {
+            StopCoroutine(spawnCoroutine);
+            spawnCoroutine = null;
+        }
+    }
+
+    private AirBubble CreateBubble() {
+        var prefab = bubblePrefabs[Random.Range(0, bubblePrefabs.Count)];
+        var obj = Instantiate(prefab, transform);
+        var bubble = obj.GetComponent<AirBubble>();
+        return bubble;
+    }
+
+    private void OnGetBubble(AirBubble bubble) {
+        bubble.gameObject.SetActive(true);
+    }
+
+    private void OnReleaseBubble(AirBubble bubble) {
+        bubble.gameObject.SetActive(false);
+    }
+
+    private void OnDestroyBubble(AirBubble bubble) {
+        Destroy(bubble.gameObject);
+    }
+}
---
+++ Assets/Code/Scripts/AirBubbles/IAirBubble.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public interface IAirBubble
+{
+    float GetAirAmount();
+    
+    void PopBubble();
+}
